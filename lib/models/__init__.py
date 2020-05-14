c3be3a7cc9de9372f335f87a9b9e2e4c479cb8c4
肖阳 <xiaoyang@xiaoyangdeMacBook-Pro.local>
2020-05-14 15:51:46 +0800

    add __init__.py

---
+++ lib/models/__init__.py
@@ -0,0 +1,19 @@
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import models.backbones.basenet
+import models.backbones.resnet
+import models.backbones.resnest
+import models.backbones.hrnet
+
+import models.nets.seg_spnet
+import models.nets.seg_pspnet
+import models.nets.seg_deeplabv3
+import models.nets.seg_danet
+import models.nets.seg_hrnet
+import models.nets.seg_hrnet_ocr
+import models.nets.seg_ocr
+import models.nets.seg_asp_ocr
+
