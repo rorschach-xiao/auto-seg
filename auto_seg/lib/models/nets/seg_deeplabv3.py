09ce4929a55216610fc86c9cf37e0b73fe3782e1
肖阳 <xiaoyang@xiaoyangdeMacBook-Pro.local>
2020-06-28 11:21:14 +0800

    update aspp module

--- auto_seg/lib/models/nets/seg_deeplabv3.py
+++ auto_seg/lib/models/nets/seg_deeplabv3.py
@@ -20,7 +20,7 @@ class DeepLabV3(BaseNet):
     def __init__(self,config,aux = True,**kwargs):
         super(DeepLabV3, self).__init__(config,**kwargs)
         self.atrous_rate = config.MODEL.ATROUS_RATE #  [6,12,18] for stride 16, [12,24,36] for stride 8
-        self.aspp = ASPP(num_classes=self.nclass,atrous_rate=self.atrous_rate,inchannel=2048,norm_layer=self.norm_layer)
+        self.aspp = ASPP(num_classes=self.nclass,atrous_rate=self.atrous_rate,inchannel=self.base_outchannel,norm_layer=self.norm_layer)
 
         self.aux = aux
         if 'hrnet' in self.backbone:
